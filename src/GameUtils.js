1308437c648b7cb91312f3140cd7e4045f91c747
showierdata9978 <68120127+showierdata9978@users.noreply.github.com>
2022-10-01 14:03:10 -0500

    hmmm, trying to import something in pure js lmao

--- src/GameUtils.js
+++ src/GameUtils.js
@@ -3,9 +3,12 @@
 const body = document.querySelector('body');
 
 const zipjs_script = document.createElement('script');
-zipjs_script.src = '"https://deno.land/x/zipjs/index.js";';
+zipjs_script.src = 'https://deno.land/x/zipjs/index.js';
+zipjs_script.type = 'module';
 
 body.appendChild(zipjs_script);
+
+
 // vm: Scratch VM (https://raw.githubusercontent.com/LLK/scratch-vm/develop/src/index.js)
 /*global vm */
 /*eslint no-undef: "error"*/
